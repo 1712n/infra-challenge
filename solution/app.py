07207ea8e22a2ec72bd8a1ed239d8b50527dba80
Daniil Novikov <ddnovikov77@gmail.com>
2023-05-10 02:33:29 +0700

    WIP: attempt at async computations, doesn't seem very successful.

--- solution/app.py
+++ solution/app.py
@@ -1,52 +1,53 @@
-import time
+import asyncio
+
+from concurrent.futures import ProcessPoolExecutor
 
 from fastapi import FastAPI
 from transformers import pipeline
 from starlette.requests import Request
 
-app = FastAPI()
 
-models = {
-    "cardiffnlp": {"path": "models/twitter-xlm-roberta-base-sentiment"},
-    "ivanlau": {"path": "models/language-detection-fine-tuned-on-xlm-roberta-base"},
-    "svalabs": {"path": "models/twitter-xlm-roberta-crypto-spam"},
-    "EIStakovskii": {"path": "models/xlm_roberta_base_multilingual_toxicity_classifier_plus"},
-    "jy46604790": {"path": "models/Fake-News-Bert-Detect"}
+app = FastAPI()
+SHARED_PROCESS_POOL = ProcessPoolExecutor(max_workers=5)
+
+MODELS = {
+    "cardiffnlp": "models/twitter-xlm-roberta-base-sentiment",
+    "ivanlau": "models/language-detection-fine-tuned-on-xlm-roberta-base",
+    "svalabs": "models/twitter-xlm-roberta-crypto-spam",
+    "EIStakovskii": "models/xlm_roberta_base_multilingual_toxicity_classifier_plus",
+    "jy46604790": "models/Fake-News-Bert-Detect"
 }
 
-for model in models.values():
-    model["pipeline"] = pipeline('text-classification', model=model["path"], device=0)
+
+async def model_inference_task(model_path, q):
+    print(f"loading {model_path}...")
+    text_classification_pipeline = pipeline('text-classification', model=model_path, device=0)
+    print(f"done loading {model_path}, waiting on the channel")
+    while True:
+        (text, response_q) = await q.get()
+        inference_result = text_classification_pipeline(text)[0]
+        await response_q.put(inference_result)
 
 
-# todo: put each model into separate process and execute inference asynchronously
+@app.on_event("startup")
+async def startup_event():
+    app.model_queues = {}
+    for model_key, model_path in MODELS.items():
+        q = asyncio.Queue()
+        app.model_queues[model_key] = q
+        asyncio.create_task(model_inference_task(model_path, q))
+
+
 @app.post("/process")
 async def process(request: Request):
     text = (await request.body()).decode()
     if not text:
         return {}
-    result = {k: None for k in models.keys()}
-    # start_overall = time.time()
-    for model_key, model_value in models.items():
-        print(f"Inference using model: {model}")
-        start = time.time()
-        result[model_key] = model_value["pipeline"](text)[0]
-        elapsed = time.time() - start
-        print(f"The result of the inference using {model_key} is {result}. It took {elapsed} seconds to compute.\n")
-    # elapsed_overall = time.time() - start_overall
-    # print(f"ELAPSED OVERALL: {elapsed_overall}")
+    result = {k: None for k in MODELS.keys()}
+    for model_key, model_q in request.app.model_queues.items():
+        response_q = asyncio.Queue()
+        await model_q.put((text, response_q))
+        result[model_key] = await response_q.get()
+        if model_key == "cardiffnlp":
+            result[model_key]["label"] = result[model_key]["label"].upper()
     return result 
-
-
-# async def server_loop(q):
-#     pipe = pipeline(model="")
-#     while True:
-#         (string, response_q) = await q.get()
-#         out = pipe(string)
-#         await response_q.put(out)
-
-
-# @app.on_event("startup")
-# async def startup_event():
-#     q = asyncio.Queue()
-#     app.model_queue = q
-#     asyncio.create_task(server_loop(q))
