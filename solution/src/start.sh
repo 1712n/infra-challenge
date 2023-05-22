00a0b59944c64ef385b7066f7f2716c6e7efe13a
Reversaidx <fedomarazm@gmail.com>
2023-05-22 16:46:47 +0200

    Update start.sh

--- solution/src/start.sh
+++ solution/src/start.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 #Activate conda
 source /opt/conda/etc/profile.d/conda.sh && conda activate base
 
-gunicorn main:app --workers=4 --bind=:8080 --worker-class=uvicorn.workers.UvicornWorker --timeout 300
+gunicorn main:app --workers=2 --bind=:8080 --worker-class=uvicorn.workers.UvicornWorker --timeout 300
