07161feb93a73f731f23c3984c7b4f19460d6bd9
vzip <zip.logon@gmail.com>
2023-06-02 13:45:41 -0600

    added more 3 workers

--- solution/dev_parallel/config.py
+++ solution/dev_parallel/config.py
@@ -10,7 +10,7 @@ config = {
         "db": ["1", "2", "3", "4", "5"]
     },
      "farm_2": {
-        "db": ["7", "8", "9", "10", "11"] 
+        "db": ["1", "8", "9", "4", "11"] 
     },
     
     "workers": [
@@ -41,6 +41,36 @@ config = {
         {
             "db": 5,
             "worker_name": "worker5",
+            "model_name": "jy46604790/Fake-News-Bert-Detect",
+            "model_labels": ["LABEL_0", "LABEL_1"] # LABEL_0: Fake news, LABEL_1: Real news
+        },
+        {
+            "db": 7,
+            "worker_name": "worker7",
+            "model_name": "cardiffnlp/twitter-xlm-roberta-base-sentiment",
+            "model_labels": ["NEGATIVE", "NEUTRAL", "POSITIVE"] # Labels for sentiment model
+        },
+        {
+            "db": 8,
+            "worker_name": "worker8",
+            "model_name": "ivanlau/language-detection-fine-tuned-on-xlm-roberta-base",
+            "model_labels": ["Arabic", "Basque", "Breton", "Catalan", "Chinese_China", "Chinese_Hongkong", "Chinese_Taiwan", "Chuvash", "Czech", "Dhivehi", "Dutch", "English", "Esperanto", "Estonian", "French", "Frisian", "Georgian", "German", "Greek", "Hakha_Chin", "Indonesian", "Interlingua", "Italian", "Japanese", "Kabyle", "Kinyarwanda", "Kyrgyz", "Latvian", "Maltese", "Mongolian", "Persian", "Polish", "Portuguese", "Romanian", "Romansh_Sursilvan", "Russian", "Sakha", "Slovenian", "Spanish", "Swedish", "Tamil", "Tatar", "Turkish", "Ukrainian", "Welsh"] # Labels for language detection model
+        },
+        {
+            "db": 9,
+            "worker_name": "worker9",
+            "model_name": "svalabs/twitter-xlm-roberta-crypto-spam",
+            "model_labels": ["HAM", "SPAM"] # Labels for crypto spam model
+        },
+        {
+            "db": 10,
+            "worker_name": "worker10",
+            "model_name": "EIStakovskii/xlm_roberta_base_multilingual_toxicity_classifier_plus",
+            "model_labels": ["LABEL_0", "LABEL_1"] # Label_1 means TOXIC, Label_0 means NOT TOXIC.
+        },
+        {
+            "db": 11,
+            "worker_name": "worker11",
             "model_name": "jy46604790/Fake-News-Bert-Detect",
             "model_labels": ["LABEL_0", "LABEL_1"] # LABEL_0: Fake news, LABEL_1: Real news
         }
--- solution/dev_parallel/tasker.py
+++ solution/dev_parallel/tasker.py
@@ -3,19 +3,33 @@ import aioredis
 import json
 import logging
 import os
+from config import config  # импортируем конфигурационный файл
 
 logging.basicConfig(level=logging.INFO)
 
 class QueueManager:
     def __init__(self):
-        self.redis_host = os.environ.get('REDIS_HOST')  
-        self.redis_port = 6379 
-        self.redis_password = os.environ.get('REDIS_PASSWORD')  
+        self.redis_host = os.environ.get('REDIS_HOST')
+        self.redis_port = 6379
+        self.redis_password = os.environ.get('REDIS_PASSWORD')
+        self.group_settings = self.load_group_settings()  # загружаем настройки групп
+        self.lock = asyncio.Lock()
+
+    def load_group_settings(self):
+        # загружаем конфигурацию групп только один раз при инициализации
+        group_settings = []
+        for key in config.keys():
+            if 'farm_' in key:
+                group_settings.append({'db_numbers': config[key]['db']})
+        return group_settings
 
     async def start(self):
         logging.info('Starting Queue Manager...')
         self.redis_source = aioredis.from_url(f"redis://{self.redis_host}:{self.redis_port}/0", password=self.redis_password)
-        self.redis_targets = [aioredis.from_url(f"redis://{self.redis_host}:{self.redis_port}/{i}", password=self.redis_password) for i in range(1, 6)]
+        self.target_groups = [
+            [aioredis.from_url(f"redis://{self.redis_host}:{self.redis_port}/{i}", password=self.redis_password) for i in group['db_numbers']]
+            for group in self.group_settings
+        ]
         logging.info('Connected to Redis.')
         await self.transfer_messages_from_redis_to_redis()
 
@@ -23,13 +37,22 @@ class QueueManager:
         await redis_db.rpush('dispatcher', message_body)
 
     async def transfer_messages_from_redis_to_redis(self, redis_queue_name='dispatcher'):
+        current_group_index = 0  # Индекс текущей группы
         while True:
-            logging.info('Waiting for message in Redis queue...')
-            _, message_body = await self.redis_source.blpop(redis_queue_name)
-            logging.info(f'Received message from Redis queue: {message_body}')
-            publish_tasks = [self.publish_message_to_redis(message_body, redis_target) for redis_target in self.redis_targets]
-            await asyncio.gather(*publish_tasks)
-            logging.info('Published message to all Redis targets.')
+            async with self.lock:
+                logging.info('Waiting for message in Redis queue...')
+                _, message_body = await self.redis_source.blpop(redis_queue_name)
+                logging.info(f'Received message from Redis queue: {message_body}')
+                if message_body:
+                    target_group = self.target_groups[current_group_index]  # Выбираем текущую группу
+                    logging.info(f'Sending message to group {current_group_index}.')
+                    publish_tasks = [self.publish_message_to_redis(message_body, redis_target) for redis_target in target_group]
+                    await asyncio.gather(*publish_tasks)
+                    logging.info(f'Published message to group {current_group_index}.')
+                    current_group_index = (current_group_index + 1) % len(self.target_groups)  # Переходим к следующей группе
+                else:
+                    logging.error('Received empty message from Redis queue.')
+                await asyncio.sleep(0.01)  # Добавляем небольшую задержку, чтобы уменьшить нагрузку на ЦП
 
 loop = asyncio.get_event_loop()
 manager = QueueManager()
