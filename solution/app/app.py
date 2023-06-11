edd7cf519a7fc4d0108f2ebf15a4679c8f8397c5
Artur <iar@napoleonit.ru>
2023-06-11 16:15:42 +0500

    setup docker-compose

--- solution/app/app.py
+++ solution/app/app.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import os
 import asyncio
 
 import uvicorn
@@ -25,6 +26,8 @@ def build_models(model_configs: List[ModelConfig], triton_url: str, batch_size: 
 
 
 config = AppConfig.parse_file("./configs/app_config.yaml")
+env_triton_url = os.getenv("TRITON_URL", None)
+config.triton_url = config.triton_url if not env_triton_url else f"{env_triton_url}:8000"
 models = build_models(config.models, config.triton_url, config.batch_size)
 
 recognition_service = TextClassificationService(models)
