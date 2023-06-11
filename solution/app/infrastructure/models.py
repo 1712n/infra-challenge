2e8472672f559a4051aea67f1212bae0b873e303
Artur <iar@napoleonit.ru>
2023-06-11 14:03:36 +0500

    application service setup

--- solution/app/infrastructure/models.py
+++ solution/app/infrastructure/models.py
@@ -15,20 +15,19 @@ class TextClassificationModelData:
 
 class TritonTextClassificationModel:
 
-    def __init__(self, name: str, triton_model_name: str, model_url: str, batch_size: int, model_labels: List[str]):
+    def __init__(self, name: str, triton_model_name: str, triton_url: str, batch_size: int, model_labels: List[str]):
         self.name = name
         self.triton_model_name = triton_model_name
-        self.model_url = model_url
+        self.triton_url = triton_url
         self.batch_size = batch_size
         self.model_labels = model_labels
-        self.model_version = 1
-        self.triton_client = triton_client = tritonclient.http.InferenceServerClient(url=self.model_url, verbose=False)
+        self.model_version = "1"
+        self.triton_client = triton_client = tritonclient.http.InferenceServerClient(url=self.triton_url, verbose=False)
         self._load_model()
         self.model_metadata = triton_client.get_model_metadata(
                 model_name=self.triton_model_name,
                 model_version=self.model_version
         )
-        self.query = tritonclient.http.InferInput(name="TEXT", shape=(self.batch_size,), datatype="BYTES")
         self.model_score = tritonclient.http.InferRequestedOutput(name="output", binary_data=False)
 
     def _load_model(self):
@@ -37,19 +36,28 @@ class TritonTextClassificationModel:
 
     def __call__(self, texts: List[str]):
         predictions = []
+        query = tritonclient.http.InferInput(name="TEXT", shape=(self.batch_size,), datatype="BYTES")
+
         for text_batch in self._perform_batches(texts):
-            self.query.set_data_from_numpy(np.asarray(text_batch, dtype=object))
-            response = self.triton_client.infer(
-                    model_name=self.triton_model_name,
-                    model_version=self.model_version,
-                    inputs=[self.query],
-                    outputs=[self.model_score]
-            )
-            logits = response.as_numpy("output")
-            batch_res = self._results_from_logits(logits)
+            if len(text_batch) != self.batch_size:
+                query = tritonclient.http.InferInput(name="TEXT", shape=(len(text_batch),), datatype="BYTES")
+
+            query.set_data_from_numpy(np.asarray(text_batch, dtype=object))
+            batch_res = self._request_model(query)
             predictions.extend(batch_res)
         predictions = [TextClassificationModelData(self.name, **prediction) for prediction in predictions]
         return  predictions
+
+    def _request_model(self, query):
+        response = self.triton_client.infer(
+                model_name=self.triton_model_name,
+                model_version=self.model_version,
+                inputs=[query],
+                outputs=[self.model_score]
+        )
+        logits = response.as_numpy("output")
+        batch_res = self._results_from_logits(logits)
+        return batch_res
 
     def _results_from_logits(self, logits: np.ndarray):
 
