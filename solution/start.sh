0b37355b95c405591db09f72e043496c2d004e32
Artur <iar@napoleonit.ru>
2023-06-15 11:34:01 +0500

    fix app start

--- solution/start.sh
+++ solution/start.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 tritonserver --model-repository=/src/triton-server/models & \
-	uvicorn app:app --reload --app-dir="$(pwd)/app/" --host 0.0.0.0 --port 8080 --workers 5
+	uvicorn app:app --app-dir="$(pwd)/app/" --host 0.0.0.0 --port 8080 --workers 5
