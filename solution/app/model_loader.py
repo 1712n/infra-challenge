231e29a60c221fa05c1a3a2e38a540ddc62605e7
Keshav Mishra <keshav.mishra0298@gmail.com>
2023-06-19 04:04:13 +0530

    add: async model load

--- solution/app/model_loader.py
+++ solution/app/model_loader.py
@@ -1,8 +1,11 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
 class ModelLoader:
     def __init__(self):
         self.models = {}
+        self.executor = ThreadPoolExecutor(max_workers=5)
 
     def load_models(self):
         model_urls = {
@@ -13,10 +16,21 @@ class ModelLoader:
             "jy46604790": "jy46604790/Fake-News-Bert-Detect"
         }
 
-        for model_name, model_url in model_urls.items():
+        def load_model(model_name, model_url):
             model = AutoModelForSequenceClassification.from_pretrained(model_url)
             tokenizer = AutoTokenizer.from_pretrained(model_url)
             self.models[model_name] = {"model": model, "tokenizer": tokenizer}
+
+        # Load models asynchronously using ThreadPoolExecutor
+        futures = []
+        for model_name, model_url in model_urls.items():
+            future = self.executor.submit(load_model, model_name, model_url)
+            futures.append(future)
+
+        # Wait for all model loading tasks to complete
+        threading.current_thread().set_name("Main")
+        for future in futures:
+            future.result()
 
     def get_models(self):
         return self.models
