0569430db4dd644a543ba8cd18a36959da16b27d
Artur <iar@napoleonit.ru>
2023-06-19 14:49:39 +0500

    pull models after build, before start

--- solution/start.sh
+++ solution/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd triton-server && git init && dvc pull && \
 tritonserver --model-repository=/src/triton-server/models & \
-	uvicorn app:app --app-dir="$(pwd)/app/" --host 0.0.0.0 --port 8080 --workers 5
+	uvicorn app:app --app-dir="/src/solution/app/" --host 0.0.0.0 --port 8080 --workers 5
